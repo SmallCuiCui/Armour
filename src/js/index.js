98cac0163e90c977c73007c9b5225a3afa5c55be
SmallCuiCui <2511692877@qq.com>
2019-05-20 21:26:07 +0800

    1

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ require(["config"],() =>{
 		//console.log(footer);
 
 		
-
+		console.log($);
 		//首页轮播图片
 		let bannerImg = ["/images/bg_scroll_1.jpg","images/bg_scroll_2.jpg","/images/bg_scroll_3.jpg"];
 		
