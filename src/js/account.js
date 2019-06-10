e39b6cdc16ddc51534a1ee8d5c923583d3acbd68
SmallCuiCui <2511692877@qq.com>
2019-06-10 12:36:17 +0800

    个人中心

--- src/js/account.js
+++ src/js/account.js
@@ -2,7 +2,8 @@ require(["config"],()=>{
 	require(["header","url","cookie","footer"],(hearder,url)=>{
 		class User{
 			constructor(){
-				this.getData();
+				// this.getData();
+				this.bindEvent();
 			}
 			getData(){
 				let userphone = $.cookie("username");
@@ -18,6 +19,23 @@ require(["config"],()=>{
 			}
 			render(){
 
+			}
+			bindEvent(){
+				let _this = this;
+				// 编辑状态下点击取消
+				$(".cancel").on("click",function(){
+					// 隐藏修改区
+					$(this).parents("ol").find("li>div").hide();
+					// 显示点击按钮
+					$(this).parents("ol").find("li:last-child").show();
+				})
+				// 点击订阅与个人信息的修改
+				$(".edit").on("click",function(){
+					// 显示修改区
+					$(this).parents("ol").find("li>div").show();
+					// 隐藏点击按钮
+					$(this).parents("ol").find("li:last-child").hide();
+				})
 			}
 		}
 		new User();
